9673ca744eed7e9dd1e23fc25db23f273d2e8947
yveszhao <yyvvs@126.com>
2017-04-20 18:35:57 +0800

    init web page

---
+++ scripts/web.js
@@ -0,0 +1,12 @@
+$(function () {
+
+      $("#slider").responsiveSlides({
+        auto: true,
+        pager: true,
+        nav: true,
+        speed: 500,
+        maxwidth: 800,
+        namespace: "centered-btns"
+      });
+
+});
